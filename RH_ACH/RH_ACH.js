51f05336039e7a1a0f3ef47f6ed8f57b34913b6a
José Slon Baltodano <jslon@icloud.com>
2015-10-15 13:49:01 -0600

    Reestructuración de las carpetas (Eliminé basura) y validación de usuario perfil y RH_ACH

--- RH_ACH/RH_ACH.js
+++
@@ -1,5 +0,0 @@
-var app = angular.module("RH_ACH",[]);
-app.controller("RH_ACH_Controller", function(){
-
-
-});
---
+++ js/app.js
@@ -0,0 +1,9 @@
+var app = angular.module('formApp', [])
+
+app.controller('MainCtrl', function ($scope) {
+  $scope.formData = {};
+
+  $scope.submitForm = function (formData) {
+    alert('Form submitted with' + JSON.stringify(formData));
+  };
+});
