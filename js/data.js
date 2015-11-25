9cbab7e2a3eeaf35a566e34e3529c64d0f48de27
mataecci <a83780@ECCI-LBW-106-16.ecci.ucr.ac.cr>
2015-11-25 17:17:49 -0600

    admin users!

--- js/data.js
+++ js/data.js
@@ -1,11 +1,32 @@
 var app =angular.module('myApp',[]);
 
 app.controller('GetUsers', function GetUsers($scope, $http) {
-
+    
+    $scope.sortType     = 'nombre';     // set the default sort type
+    $scope.sortReverse  = false;        // set the default sort order
+    $scope.searchUser   = '';           // set the default search/filter term
+    $scope.id           = '';
+    $scope.correo       = '';
+    $scope.nombre       = '';           
+    $scope.apellido_I   = '';
+    $scope.apellido_II  = '';
+    $scope.rol          = '';
+    $scope.universidad  = '';
+    
+    
     // this is where the JSON is consumed
-    $http.get("http://localhost/REDIES/php/administrador/users.php").
+    $http.get("../php/administrador/users.php").
         success(function(data) {
             // here the data from the api is assigned to a variable named users
             $scope.users = data;
         });
+        
+    $scope.editUsr = function(id){
+        $scope.nombre       = $scope.users[id-1].nombre;
+        $scope.apellido_I   = $scope.users[id-1].apellido_I;
+        $scope.apellido_II  = $scope.users[id-1].apellido_II;
+        $scope.correo       = $scope.users[id-1].correo;
+        $scope.rol          = $scope.users[id-1].rol;
+        $scope.universidad  = $scope.users[id-1].nombre_u;
+    }
 });
--- php/administrador/users.php
+++ php/administrador/users.php
@@ -1,5 +1,5 @@
 <?php
-
+	
 	session_start();
 
   	if(!$_SESSION){
@@ -8,19 +8,22 @@
 		 </script>';
 	}
 
-	try{
+	try{		
 		$conn = new PDO('mysql:host=redies.cr;dbname=redies_indicadores', 'redies', 'Acceso123!');
 		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-		$sql = 'SELECT u.nombre, u.apellido_I, u.apellido_II, u.correo, d.telefono, d.nombre_u, u.rol FROM usuarios u, universidad d WHERE u.universidad_id = d.id';
+	
+		$sql = 'SELECT @row_num := @row_num + 1 as usr, u.nombre, u.apellido_I, u.apellido_II, u.correo, u.tel_usuario, u.tel_movil, d.nombre_u, u.rol FROM usuarios u, universidad d JOIN (SELECT @row_num := 0) r WHERE u.universidad_id = d.id';
 		$stmt = $conn->prepare($sql);
 		$stmt->execute();
+		
+		// fetch the results into an array
+    $result = $stmt->fetchAll( PDO::FETCH_ASSOC );
+    
+    // convert to json
+    $json = json_encode( $result );
 
-        $result = $stmt->fetchAll( PDO::FETCH_ASSOC );
-
-        $json = json_encode( $result );
-
-        echo $json;
+    // echo the json string
+    echo $json;
 	}
 	catch(PDOException $excp){
 		echo 'error: ' . $excp->getMessage();
